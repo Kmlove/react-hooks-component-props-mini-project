5baeb865406028763bdb29b7f264c3977fedd4a0
Kim Love <kimberlymlove15@gmail.com>
2023-08-22 19:45:48 -0400

    Bonus Feature: Minutes to Read completed

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,37 @@
-function Article ({
-    title,
-    date = "January 1, 1970",
-    preview
-}){
-    return (
-        <article>
-            <h3>{title}</h3>
-            <small>{date}</small>
-            <p>{preview}</p>
-        </article>
-    )
+function Article({ 
+    title, 
+    date = "January 1, 1970", 
+    preview, 
+    minutes 
+}) {
+  
+    function readTime() {
+        let display = minutes < 30 ? "☕️" : "🍱";
+        let time = minutes;
+
+        if (minutes < 30) {
+        while (time > 5) {
+            display += "☕️";
+            time -= 5;
+        }
+        return display;
+
+        } else {
+        while (time > 10) {
+            display += "🍱";
+            time -= 10;
+        }
+        return display;
+        }
+    }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date} - {readTime()}{minutes} min read</small>
+      <p>{preview}</p>
+    </article>
+  );
 }
 
-export default Article
+export default Article;
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,11 +1,10 @@
-import blogData from "../data/blog"
 import Article from "./Article"
 
 function ArticleList({posts}){
 
     console.log(posts)
     const articleArray = posts.map((blogObj) => {
-        return <Article key={blogObj.id} title={blogObj.title} date={blogObj.date} preview={blogObj.preview}/>
+        return <Article key={blogObj.id} minutes={blogObj.minutes} title={blogObj.title} date={blogObj.date} preview={blogObj.preview}/>
     })
     
     return (
